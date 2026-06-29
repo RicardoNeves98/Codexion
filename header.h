ffb63ebf07acf391bb9c7a334a99d6ae83f5c6f7
Ricardo Carmo Neves <rcarmo-n@c1r14s8.42porto.com>
2026-06-29 17:06:17 +0100

    updating files

--- header.h
+++ header.h
@@ -4,19 +4,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct coder
+typedef struct s_list
 {
-    int coder_num;
     int time_to_burnout;
     int time_to_compile;
     int time_to_debug;
     int time_to_refactor;
-    int number_of_compiles;
-}   coder;
+    int nb_of_comp;
+    int nb_of_comp_required;
+}   t_list;
 
 typedef struct monitor
 {
     int dongle_cooldown;
-    int number_of_compiles_required;
     struct coder    *coder_info;
 }   monitor;
--- main.c
+++ main.c
@@ -1,62 +1,77 @@
 #include "codexion.h"
 
-void display_error(char **argv, int i)
+int *display_error(char **argv, int i)
 {
     char *args[8] = {
         "number_of_coders", "time_to_burnout", "time_to_compile",
         "time_to_debug", "time_to_refactor", "number_of_compiles_required",
         "dongle_cooldown", "scheduler"
-    }
-    if (i <= 7)
-        printf("[ERROR] '%s' should be a positiver integer\n", args[i])
+    };
+
+    if (i < 8)
+        printf("[ERROR] '%s' should be a positiver integer\n", args[i]);
     if (i == 8)
-        printf("[ERROR] '%s' should be either 'fifo' or 'edf'\n", args[i])
+        printf("[ERROR] 'scheduler' should be either 'fifo' or 'edf'\n");
+    return (NULL);
 }
 
-int parsing_args(char **argv, int *arr)
+int *parsing_args(char **argv)
 {
     int i;
     int num;
+    int *arr;
 
-    i = -1;
-    while (++i <= 8)
+    i = 0;
+    arr = malloc(8 * sizeof(*arr));
+    if (!arr)
+        return (NULL)
+    while (i < 8)
     {
         num = atoi(argv[i]);
         if (num <= 0)
-        {
-            display_error(argv, i);
-            return (free(arr), 0);
-        }
+            return (display_error(argv, i));
         else:
-            arr[i] = num;
+            arr[i++] = num;
     }
-    if (strcmp(argv[i], "fifo") == 0)
-        arr[i] = 0;
-    else if (strcmp(argv[i], "edf") == 0)
-        arr[i] = 1;
+    if (strcmp(argv[8], "fifo") == 0)
+        arr[8] = 0;
+    else if (strcmp(argv[8], "edf") != 0)
+        arr[8] = 1;
     else
-    {
-        display_error(argv, i);
-        return (free(arr), 0);
-    }
-    return (1);
+        display_error(argv, 8);
+    return (arr);
+}
+
+int get_coder_info(int *arr, t_list *coder_info)
+{
+    coder_info->time_to_burnout = arr[1];
+    coder_info->time_to_compile = arr[2];
+    coder_info->time_to_debug = arr[3];
+    coder_info->time_to_refactor = arr[4];
+    coder_info->nb_of_comp = arr[5];
+    coder_info->nb_of_comp_required = arr[6];
+    coder_info->scheduler = arr[7];
 }
 
 int main(int argc, char **argv)
 {
+    int i;
     int *arr;
+    t_list *coder_info;
+    pthread_t *threads;
 
+    i = -1;
     if (argc != 9)
-    {
-        printf("[ERROR] Requires 9 arguments to run\n");
-        return (1);
-    }
-    arr = malloc(8 * sizeof(int));
+        return (printf("[ERROR] Needs 9 arguments\n"), 1);
+    arr = parsing_args(argv);
     if (!arr)
-    {
-        printf("[ERROR] Allocation of memory failed\n");
         return (1);
-    }
-    if (!parsing_args(arr))
-        return (1);
+    coder_info = malloc(sizeof(*coder_info));
+    threads = malloc(arr[0] * sizeof(*threads));
+    if (!coder_info || !threads)
+        return (printf("[ERROR] Memory allocation failed\n"), 1);
+    get_coder_info(arr, coder_info)
+    while (++i < arr[0])
+        if pthread_create(threads[i], NULL, function, coder_info)
+            break;
 }
